34235dbbc4c7b467a8f7841f90eeeaa5866b04ac
BrunoPdOliveir4 <bpedrosodroliveira10@gmail.com>
2025-04-08 11:04:47 -0300

    Add to readme new feature

--- src/assets/MailHandle.js
+++ src/assets/MailHandle.js
@@ -3,28 +3,24 @@ const formatMail = (body) => {
       name,
       company,
       jobPosition: jobPosition,
+      contact,
       hours: hoursWeek,
       income
     } = body;
   
     return {
       subject: getSubject(jobPosition, company),
-      text: getText(name, company, jobPosition, hoursWeek, income)
+      text: getText(name, company, jobPosition, hoursWeek, income, contact)
     };
   };
   
   const getSubject = (jobPosition, company) =>
     `Vaga: ${jobPosition} in ${company}`;
   
-  const getText = (
-    name = "N/A",
-    company = "N/A",
-    jobPosition = "N/A",
-    hoursWeek = "N/A",
-    income = "N/A"
-  ) => 
+  const getText = (name, company, jobTitle, hoursWeek, income, contact) =>
     `Prazer, meu nome é ${name}. Trabalho para a empresa: ${company}.
-  Te convido para a vaga de ${jobPosition}. Esta oportunidade é de ${hoursWeek}h/semana, recebendo uma renda mensal de: R$${income}`;
+  Te convido para a vaga de ${jobTitle}. Esta oportunidade é de ${hoursWeek}h/semana, com renda mensal de R$${income}.
+  Para mais informações, você pode me contatar via: ${contact}`;
   
   module.exports = { formatMail };
   
